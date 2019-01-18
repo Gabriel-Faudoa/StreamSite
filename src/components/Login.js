91aa04715ef0842e17f583def91646970ea677e1
Gabriel Elias Faudoa <Gabrielfaudoa@icloud.com>
2019-01-18 15:35:45 -0700

    Set Up Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,27 @@ import React, { Component } from "react";
 class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="Forms">
+      <h1 className="Loginh1">Login</h1>
+        <form>
+          <label>
+            Email
+            <br/>
+            <input type="text" name="Email" />
+          </label>
+          <div>
+          <label>
+            Password
+            <br/>
+            <input type="text" name="Password" />
+          </label>
+          </div>
+          <button className="btn btn-secondary my-2 my-sm-0">Login</button>
+          <br/>
+          Or
+          <br/>
+        </form>
+        <a href="./Signup"><button className="btn btn-secondary my-2 my-sm-0">Create User</button></a>
       </div>
     );
   }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,7 +4,7 @@ class Signup extends Component {
   render() {
     return (
       <div>
-
+        <h1>Sign Up</h1>
       </div>
     );
   }
