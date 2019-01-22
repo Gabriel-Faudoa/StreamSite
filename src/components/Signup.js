2395c2d2643c9c80224f0c00eb87573b52af6865
Gabriel Elias Faudoa <Gabrielfaudoa@icloud.com>
2019-01-22 15:03:45 -0700

    Signup Page Setup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,8 +3,35 @@ import React, { Component } from "react";
 class Signup extends Component {
   render() {
     return (
-      <div>
-        <h1>Sign Up</h1>
+      <div className="Forms">
+        <h1 className="Loginh1">Sign Up</h1>
+        <form>
+        <label>
+            First Name
+            <br/>
+            <input type="text" name="First Name" />
+          </label>
+          <br/>
+          <label>
+            Last Name
+            <br/>
+            <input type="text" name="Last Name" />
+          </label>
+          <br/>
+          <label>
+            Email
+            <br/>
+            <input type="text" name="Email" />
+          </label>
+          <div>
+          <label>
+            Password
+            <br/>
+            <input type="text" name="Password" />
+          </label>
+          </div>
+          <button className="btn btn-secondary my-2 my-sm-0">Login</button>
+        </form>
       </div>
     );
   }
