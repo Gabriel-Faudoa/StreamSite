e2816607c7aa98445fca74d063cd47749725322e
Gabriel Elias Faudoa <Gabrielfaudoa@icloud.com>
2019-01-24 19:03:51 -0700

    Updated Schedule Page and Added Site Icon

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,8 +3,33 @@ import React, { Component } from "react";
 class Schedule extends Component {
   render() {
     return (
-      <div className="">
-        <h1>To be determined</h1>
+      <div>
+        <p className="Forms">
+          Stream times not guranteed but will try to stick to schedule as best as possible
+        </p>
+        <ul className="Ul">
+          <li>
+            Monday: No Stream
+          </li>
+          <li>
+            Tuesday: 7 pm to 10ish pm
+          </li>
+          <li>
+            Wednesday: No Stream
+          </li>
+          <li>
+            Thursday: 7 pm to 10ish pm
+          </li>
+          <li>
+            Friday: 7 pm to 10ish pm
+          </li>
+          <li>
+            Saturday: No Stream
+          </li>
+          <li>
+            Sunday: 9 pm to 12 am
+          </li>
+        </ul>
       </div>
     );
   }
